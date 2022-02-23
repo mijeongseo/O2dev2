35a359e2965a980c6b146a2a2ec7bf78e5897651
mijeongseo <mimiseo@naraspace.com>
2022-02-23 17:15:20 +0900

    amplify import auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "o2dev430ab3a1": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
